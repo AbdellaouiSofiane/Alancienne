e831405320e156c0f5b1c852145c080e9dc4e19f
sofiane abdellaoui <sofiane.abdellaoui.esb@gmail.com>
2021-03-02 02:06:31 +0100

    handle realtime cart update with ajax

--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from .views import ProductCreateView, cart
+from .views import ProductCreateView, cart, cart_update_ajax
 
 
 urlpatterns = [
     path('', ProductCreateView.as_view(), name='product_create'),
     path('cart/', cart, name='cart'),
+    path('cart_update/', cart_update_ajax, name='cart_update'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -2,12 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.messages.views import SuccessMessageMixin
-
+from django.http import JsonResponse
 
 from .models import Product, Item, Cart
 from .forms import ProductCreateForm
-
-
 
 
 class ProductCreateView(SuccessMessageMixin, CreateView):
@@ -16,7 +14,6 @@ class ProductCreateView(SuccessMessageMixin, CreateView):
 	template_name = 'shop/product_create.html'
 	success_url = reverse_lazy('product_create')
 	success_message = "Le produit a été ajouter avec succès"
-
 
 
 def cart(request):
@@ -29,3 +26,16 @@ def cart(request):
 		for product in Product.objects.all():
 			Item.objects.create(cart=cart, product=product)
 	return render(request, 'shop/cart.html', {'cart': cart})
+
+def cart_update_ajax(request):
+	if request.method == 'POST' and request.is_ajax():
+		item = Item.objects.get(id=request.POST['item_id'])
+		item.quantity = request.POST.get('item_quantity', 0)
+		item.save()
+		response = {
+			'total_price' : item.cart.total_ttc(),
+			'total_quantity' : item.cart.total_quantity()
+		}
+		return JsonResponse(response, status=200)
+	else :
+		return JsonResponse({'error': 'failed'})
